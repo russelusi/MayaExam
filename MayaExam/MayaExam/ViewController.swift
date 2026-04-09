14e33aa8477281c81e884c9f04545968195df809
John Russel Usi <johnrusselusi@ymail.com>
2026-04-09 21:22:03 +0800

    Initial project files.

---
+++ MayaExam/MayaExam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MayaExam
+//
+//  Created by John Russel Usi on 4/9/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MayaExam/MayaExamTests/MayaExamTests.swift
@@ -0,0 +1,17 @@
+//
+//  MayaExamTests.swift
+//  MayaExamTests
+//
+//  Created by John Russel Usi on 4/9/26.
+//
+
+import Testing
+@testable import MayaExam
+
+struct MayaExamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
